934b38163f8ba027188c5aadc80b8ac83d16aa00
Tanmay <tanmay@Tanmays-MacBook-Pro.local>
2026-09-10 17:56:36 +0530

    Externalize DB credentials, untrack IDE files

--- src/main/java/com/moviebooking/db/DBConnection.java
+++ src/main/java/com/moviebooking/db/DBConnection.java
@@ -1,16 +1,30 @@
 package com.moviebooking.db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/moviebooking";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1234";
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = DBConnection.class.getClassLoader()
+                .getResourceAsStream("db.properties")) {
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load db.properties", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.username"),
+                props.getProperty("db.password")
+        );
     }
 }
